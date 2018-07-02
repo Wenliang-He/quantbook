c2afc6d1e6c8bf1db9ff7c6ae2633e63b86ffda6
Wenliang-He <hewenliang@gmail.com>
2018-07-02 16:37:07 +0800

    Add a folder

--- _deploy.sh
+++ _deploy.sh
@@ -5,12 +5,12 @@ set -e
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
-git config --global user.email "hewenliang@gmail.com"
 git config --global user.name "Wenliang-He"
+git config --global user.email "hewenliang@gmail.com"
 
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
 cd book-output
 cp -r ../_book/* ./
 git add --all *
-git commit -m"Update the book" || true
+git commit -m "Update the book" || true
 git push -q origin gh-pages
